c1893f22df0ff74ec7420285dfa710ae167d36fc
138bit <47329030+138bit@users.noreply.github.com>
2025-08-26 14:17:11 +0200

    Assignment 4.1: Threading

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -3,10 +3,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
-#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+#include <stdio.h>
+#include <errno.h>
+#include <time.h>
+
+#define PROG_NAME "thread"
+#define DEBUG
+#ifdef DEBUG
+#define dbg(fmt, ...) \
+	printf("-- %s: %s: " fmt " \n", PROG_NAME, __func__, ##__VA_ARGS__)
+#else
+#define dbg(fmt, ...)
+#endif
 
 void* threadfunc(void* thread_param)
 {
@@ -14,6 +22,29 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+	struct thread_data* th_data = (struct thread_data *) thread_param;
+	th_data->thread_complete_success = false;
+
+	// sleep and lock
+	if (usleep(th_data->wait_obtain * 1000)) {
+		perror("threadfunc: usleep (1)");
+		goto out_unlocked;
+	}
+	pthread_mutex_lock(th_data->mutex);
+
+	// sleep and unlock (eventually)
+	if (usleep(th_data->wait_release * 1000)) {
+		perror("threadfunc: usleep (1)");
+		goto out_locked;
+	}
+
+	th_data->thread_complete_success = true;
+
+out_locked:
+	pthread_mutex_unlock(th_data->mutex);
+out_unlocked:
+	dbg("Exiting..");
     return thread_param;
 }
 
@@ -28,6 +59,23 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+	dbg("input data: obt:%d / wait: %d", wait_to_obtain_ms, wait_to_release_ms);
+	struct thread_data *th_data = malloc(sizeof(struct thread_data));
+	int ret;
+
+	th_data->mutex = mutex;
+	th_data->wait_obtain = wait_to_obtain_ms;
+	th_data->wait_release = wait_to_release_ms;
+
+	ret = pthread_create(thread, NULL, threadfunc, (void*)th_data);
+
+	if (ret) {
+		perror("start_thread_obtaining_mutex: pthread_create");
+		exit (1);
+	}
+
+	fprintf(stderr, "Thread exited with %d", ret);
+
+	return true;
 }
 
