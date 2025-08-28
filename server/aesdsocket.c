691585878178e8de6891377431a22f1ae6726d51
138bit <47329030+138bit@users.noreply.github.com>
2025-08-28 19:51:24 +0200

    Assignment6: Threaded aesd server

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -7,9 +7,41 @@
 #include <sys/socket.h>	/* socket	*/
 #include <arpa/inet.h>	/* inet_ntop	*/
 #include <signal.h>	/* sigaction */
+#include <pthread.h>
+#include <time.h>
 
 #define PROG_NAME	"aesdsocket"
 
+// Global data
+FILE *log_fp;
+pthread_mutex_t mutex;
+
+void global_clean() {
+	if (log_fp > 0) fclose(log_fp);
+
+	/* I don't have to check this, because if pthread_mutex_init
+	 * fails the program exists */
+	pthread_mutex_destroy(&mutex);
+}
+
+void global_setup() {
+	log_fp = fopen("/var/tmp/aesdsocketdata", "w+");
+	if (!log_fp) {
+		perror("fopen failed");
+		exit(1);
+	}
+	if (pthread_mutex_init(&mutex, NULL)) {
+		perror("pthread_mutex_init failed");
+		goto err;
+	}
+	return;
+err:
+	global_clean();
+	exit(1);
+
+}
+
+// Server
 typedef struct {
 	int sock;
 	struct sockaddr_in info;
@@ -44,12 +76,23 @@ void server_setup(server_t *s) {
 		goto err;
 	}
 
-	if (bind(s->sock, (struct sockaddr *)&s->info, sizeof(s->info))) {
-		perror("bind failed");
-		goto err;
+	{ /* The tests starts this service too fast. Hence we iterativly attempt to bind
+	   * until the kernel frees the port. */
+		int i = 1, j = 1024;
+		for (; i <= j; i++) {
+			if (bind(s->sock, (struct sockaddr *)&s->info, sizeof(s->info))) {
+				usleep(i * 100*1000);
+			} else {
+				break;
+			}
+			if (i == j) {
+				perror("bind failed");
+				goto err;
+			}
+		}
 	}
 
-	if (listen(s->sock, 10)) {
+	if (listen(s->sock, 20)) {
 		perror("listen failed");
 		goto err;
 	}
@@ -60,7 +103,7 @@ err:
 	exit(1);
 }
 
-// Struct to control client.
+// Client
 typedef struct {
 	int sock;
 	struct sockaddr_in addr;
@@ -69,16 +112,7 @@ typedef struct {
 	socklen_t addrlen;
 } client_t;
 
-void client_close(client_t *c) {
-	fclose(c->fp);
-}
-
 void client_setup(client_t *c) {
-	c->fp = fopen("/var/tmp/aesdsocketdata", "w+");
-	if (!c->fp) {
-		perror("fopen failed");
-		exit(1);
-	}
 	c->addrlen = sizeof(c->addr);
 	memset(c->ipaddr, 0, INET_ADDRSTRLEN);
 }
@@ -91,7 +125,7 @@ void client_logic(client_t *c) {
 	ssize_t sendsocklen = 0;
 
 	// Always make sure we're writing to the end of the file
-	if (fseek(c->fp, 0, SEEK_END)) {
+	if (fseek(log_fp, 0, SEEK_END)) {
 		perror("fseek END failed");
 		goto err;
 	}
@@ -102,25 +136,32 @@ void client_logic(client_t *c) {
 		goto err;
 	}
 	
+
+	if (pthread_mutex_lock(&mutex)) {
+		perror("pthread_mutex_lock failed");
+		goto err;
+	}
 	syslog(LOG_INFO, "Accepted connection from %s\n", c->ipaddr);
+
 
 	// Read from socket and write to file
 	while ((readsocklen = recv(c->sock, buf, BUFLEN, MSG_DONTWAIT)) > 1) {
-
 		buf[readsocklen] = '\0';
-		if (fputs(buf, c->fp) < 0) {
+		syslog(LOG_INFO, "%s", buf);
+		if (fputs(buf, log_fp) < 0) {
 			perror("fputs failed");
 			goto err;
 		}
 	}
 
-	if (fseek(c->fp, 0, SEEK_SET)) {
+	if (fseek(log_fp, 0, SEEK_SET)) {
 		perror("fseek SET failed");
 		goto err;
 	}
+	fflush(log_fp);
 
 	// Read from the file and write to the socket
-	while ((readfilelen = fread(buf, sizeof(char), BUFLEN, c->fp))) {
+	while ((readfilelen = fread(buf, sizeof(char), BUFLEN, log_fp))) {
 		sendsocklen = send(c->sock, buf, readfilelen, 0);
 
 		if (readfilelen != sendsocklen) {
@@ -131,6 +172,9 @@ void client_logic(client_t *c) {
 	}
 
 err:
+	if (pthread_mutex_unlock(&mutex)) {
+		perror("pthread_mutex_unlock failed");
+	}
 	shutdown(c->sock, SHUT_RDWR);
 	close(c->sock);
 	syslog(LOG_INFO, "Closed connection from %s", c->ipaddr);
@@ -138,8 +182,8 @@ err:
 
 // sigaction
 static int program_active = 1;
-
 static void sig_handler(int signal) {
+	// https://man7.org/linux/man-pages/man7/signal-safety.7.html
 	switch (signal) {
 		case SIGINT:
 		case SIGTERM:
@@ -165,11 +209,78 @@ void signal_setup(struct sigaction *a) {
 	}
 }
 
+// Thread
+typedef struct task_t_ {
+	pthread_t tid;
+	void **value_ptr;
+	struct task_t_ *next;
+	client_t clnt;
+} task_t;
+
+static task_t *head;
+
+static void * thread_run(void *arg) {
+	client_t *clnt = (client_t *)arg;
+	client_logic(clnt);
+	//pthread_detach(pthread_self());
+	pthread_exit(NULL);
+}
+
+void thread_clean() {
+	static task_t *next;
+	int x = 0;
+
+	while(head) {
+		printf("cleanup %d\n", x++);
+		next = head->next;
+		pthread_join(head->tid, NULL);
+		free(head);
+		head = NULL;
+		head = next;
+	}
+}
+
+static void * timer_log(void *arg) {
+	time_t wall_time;
+	char * timestamp;
+	char msg[512];
+
+	while(program_active) {
+		// sleep 10 seconds
+		sleep(10);
+		
+		if (pthread_mutex_lock(&mutex)) {
+			perror("pthread_mutex_lock error");
+			// do something?
+		}
+
+		fflush(log_fp);
+		if (fseek(log_fp, 0, SEEK_END)) {
+			perror("fseek END failed");
+		}
+
+		// write timestamp to file
+		wall_time = time(NULL);
+		timestamp = asctime(localtime(&wall_time));
+
+		memset(msg, 0, 512);
+		sprintf(msg, "timestamp:%s", timestamp);
+		fwrite(msg, sizeof(char), strlen(msg), log_fp);
+		fflush(log_fp);
+
+		if (pthread_mutex_unlock(&mutex)) {
+			perror("pthread_mutex_unlock error");
+			// do something?
+		}
+	}
+	pthread_exit(NULL);
+}
+
 int main(int argc, char *argv[]) {
 	server_t serv;
-	client_t clnt;
 	struct sigaction actn;
 	int ret = 0;
+	pthread_t log_tid;
 
 	if (argc > 1) {
 		if (strncmp(argv[1], "-d", 2)) {
@@ -180,22 +291,45 @@ int main(int argc, char *argv[]) {
 	}
 
 	openlog(PROG_NAME, LOG_PERROR|LOG_PID, LOG_USER);
-	signal_setup(&actn);
+	global_setup();
 	server_setup(&serv);
-	client_setup(&clnt);
-	
-	while (program_active) {
-		clnt.sock = accept(serv.sock, (struct sockaddr *)&clnt.addr, &clnt.addrlen);
-		if (clnt.sock < 0) {
-			ret = errno != EINTR;
-			if (ret) perror("accept failed");
-			break;
-		}
-		client_logic(&clnt);
+
+	if (pthread_create(&log_tid, NULL, timer_log, NULL)) {
+		perror("pthread_create LOG failed");
+		goto out;
 	}
 
-	client_close(&clnt);
+	signal_setup(&actn);
+	head = NULL;
+	
+	while (program_active) {
+		task_t *next = malloc(sizeof(task_t));
+		next->next = head;
+		head = next;
+
+		memset(next, 0, sizeof(task_t));
+		client_setup(&next->clnt);
+		next->clnt.sock = accept(serv.sock, (struct sockaddr *)&next->clnt.addr, &next->clnt.addrlen);
+
+		if (next->clnt.sock < 0) {
+			ret = errno != EINTR;
+			if (ret) perror("accept failed");
+			goto out;
+		}
+
+		if (pthread_create(&next->tid, NULL, thread_run, &next->clnt)) {
+			ret = errno != EINTR;
+			if (ret) perror("pthread_create CLNT failed");
+			goto out;
+		}
+	}
+out:
+	pthread_join(log_tid, NULL);
+
+	global_clean();
 	server_close(&serv);
+	thread_clean();
+
 	closelog();
 	return ret;
 }
