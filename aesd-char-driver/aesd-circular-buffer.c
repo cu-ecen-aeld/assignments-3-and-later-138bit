2a00d9dae394e8f93515cf85624be570f25d8b01
138bit <47329030+138bit@users.noreply.github.com>
2025-08-29 18:14:03 +0200

    Assignment7.1: aesd-circular-buffer.c

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -16,6 +16,10 @@
 
 #include "aesd-circular-buffer.h"
 
+#include <stdio.h>
+
+#define INCREASE_OFFS(n) n = ((n + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
  * @param char_offset the position to search for in the buffer list, describing the zero referenced
@@ -31,8 +35,33 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 {
     /**
     * TODO: implement per description
+	if char_offset > strlen(), increase out and  char_offset -= strlen
     */
-    return NULL;
+	uint8_t start = buffer->out_offs;
+
+	printf("input: out_offs = %d, char_offset = %lu\n", start, char_offset);
+	
+	// Catch "empty" buffers
+	if (start == buffer->in_offs && !buffer->full) {
+		printf("Buffer is empty. Exiting..\n");
+		return NULL;
+	} 
+
+	while (char_offset >= buffer->entry[start].size) {
+		printf(" ... %lu > %lu ... \n", char_offset, buffer->entry[start].size);
+		char_offset -= buffer->entry[start].size;
+		INCREASE_OFFS(start);
+
+		if (start == buffer->in_offs) {
+			printf("char_offset too long!\n");
+			return NULL;
+		}
+	}
+	*entry_offset_byte_rtn = char_offset;
+	printf("Returning out_offs (%d) with offset %lu (str = %s, size = %lu)\n",
+		start, char_offset, buffer->entry[start].buffptr, buffer->entry[start].size);
+
+    return &buffer->entry[start];
 }
 
 /**
@@ -47,6 +76,23 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+	if (buffer->full) {
+		// If buffer is full, increase out_offs
+		//buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+		printf("increasing out_offs to %d\n", buffer->out_offs);
+		INCREASE_OFFS(buffer->out_offs);
+	}
+
+	printf("Pos %d set to %s\n", buffer->in_offs, add_entry->buffptr);
+
+	// Add entry to the array and increase in_offs
+	memcpy(&buffer->entry[buffer->in_offs], add_entry, sizeof(struct aesd_buffer_entry));
+	//buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+	INCREASE_OFFS(buffer->in_offs);
+
+	// Check if buffer is full on the last line to make sure the starting condition doesn't mark the
+	// buffer as full from the start.
+	buffer->full = buffer->in_offs == buffer->out_offs;
 }
 
 /**
